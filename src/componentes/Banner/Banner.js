783990f260e2e574e6d2a82f3006a49b1d66bb1b
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-08-02 16:24:22 -0300

    import Banner.css

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,7 +1,12 @@
+import './Banner.css'
+
 function Banner(){
 //  Isso é um JSX - parece um HTML mas não é.
+// é como o react interpreta e converte para o DOM.
     return(
+    <header className="banner">
     <img src="/imagens/banner.png" alt="banner principal da page"/>
+    </header>
  )
 }
 
