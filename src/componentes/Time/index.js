9839c76b689c744cf090751e22beeb6f8df009d6
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-08-07 14:03:11 -0300

    create const css and borderColor

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,13 @@
 import './Time.css'
 
 const Time = (props) =>{
+
+    const css = { backgroundColor: props.corSecundaria}
+    const borderColor = {borderColor: props.corPrimaria}
+    
     return(
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        <section className='time' style={css}>
+            <h3 style={borderColor}>{props.nome}</h3>
         </section>
     )
 }
