cdbbf89746b1f023a49ffafbf538d25b2f7f9775
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-08-09 02:07:37 -0300

    create corDeFundo prop

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,15 +1,29 @@
-import './Time.css'
+import Colaborador from "../Colaborador";
+import "./Time.css";
 
-const Time = (props) =>{
+const Time = (props) => {
+  const css = { backgroundColor: props.corSecundaria };
+  const borderColor = { borderColor: props.corPrimaria };
 
-    const css = { backgroundColor: props.corSecundaria}
-    const borderColor = {borderColor: props.corPrimaria}
-    
-    return(
-        <section className='time' style={css}>
-            <h3 style={borderColor}>{props.nome}</h3>
-        </section>
-    )
-}
+  return (
+    // pode usar o ternario "?" ":" ou && (serve como um verificador)
+    // caso seja true então faça o a seguir.
+    (props.colaboradores.length > 0) && <section className="time" style={css}>
+      <h3 style={borderColor}>{props.nome}</h3>
 
-export default Time
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            corDeFundo={props.corPrimaria}
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Time;
